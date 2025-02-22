0056e6f2c5d2ccdfad01c440cc3b20efae8af746
Sarina-R <Sarina.r.n.0@gmail.com>
2025-02-22 17:27:44 +0330

    loading

--- app/(main)/home/(homePageComponents)/Leagues.tsx
+++ app/(main)/home/(homePageComponents)/Leagues.tsx
@@ -7,6 +7,7 @@ import { API_URLS } from "@/app/api/url";
 import { Card } from "@/components/ui/card";
 import Image from "next/image";
 import axios from "axios";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface EventData {
   title: string;
@@ -39,7 +40,66 @@ const Leagues = () => {
   }, []);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <>
+        <div className="mt-4 space-y-8 p-0 lg:p-6">
+          <div className=" hidden sm:block">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-6 gap-x-3">
+              {[...Array(3)].map((_, index) => (
+                <Card key={index} className="p-4 space-y-4">
+                  <Skeleton className="w-full h-48 rounded-lg" />
+                  <Skeleton className="h-6 w-3/4" />
+                  <Skeleton className="h-4 w-1/2" />
+                  <div className="flex gap-2">
+                    {[...Array(2)].map((_, idx) => (
+                      <Skeleton key={idx} className="h-6 w-16 rounded-full" />
+                    ))}
+                  </div>
+                  <Skeleton className="h-5 w-1/3" />
+                  <div className="flex gap-4">
+                    <Skeleton className="h-10 w-1/2" />
+                    <Skeleton className="h-10 w-1/2" />
+                  </div>
+                </Card>
+              ))}
+            </div>
+          </div>
+
+          <div className="sm:hidden space-y-4">
+            {[...Array(3)].map((_, index) => (
+              <Card
+                key={index}
+                className="max-w-xl flex p-4 gap-4 shadow-lg sm:hidden"
+              >
+                <div className="w-32 h-20 relative">
+                  <Skeleton className="w-full h-full rounded-lg" />
+                </div>
+
+                <div className="flex flex-col flex-grow space-y-2">
+                  <Skeleton className="h-6 w-3/4" />
+                  <Skeleton className="h-4 w-1/2" />
+                  <div className="bg-neutral-100 dark:bg-neutral-900 p-2 rounded-md mt-2 flex justify-between text-sm">
+                    <div className="flex flex-col items-center">
+                      <span className="text-[0.7rem] px-2">Age Category</span>
+                      <Skeleton className="h-4 w-12 rounded-full" />
+                    </div>
+                    <div className="flex flex-col items-center justify-center">
+                      <span className="text-[0.7rem] px-2">Registration</span>
+                      <Skeleton className="h-5 w-16 rounded-full" />
+                    </div>
+                  </div>
+                  <div className="mt-3 flex gap-2 justify-between">
+                    <Skeleton className="h-8 w-14 rounded-md" />
+                    <Skeleton className="h-8 w-16 rounded-md" />
+                    <Skeleton className="h-8 w-20 rounded-md" />
+                  </div>
+                </div>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </>
+    );
   }
 
   return (
