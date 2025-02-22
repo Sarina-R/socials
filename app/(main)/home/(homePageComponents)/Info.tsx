cb1af51f83df34683e6b57df9da55bc1dcbd8965
Sarina-R <Sarina.r.n.0@gmail.com>
2025-02-22 11:23:37 +0330

    loading

--- app/(main)/home/(homePageComponents)/Info.tsx
+++ app/(main)/home/(homePageComponents)/Info.tsx
@@ -5,6 +5,9 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Image from "next/image";
 import { Card, CardTitle } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { CalendarDays } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface EventData {
   poster: string;
@@ -14,6 +17,12 @@ interface EventData {
   event_dates: { start: string; end: string };
   city: string;
   event_type: string;
+  organizers: organizers[];
+}
+
+interface organizers {
+  name: string;
+  logo: string;
 }
 
 const Info = () => {
@@ -26,7 +35,7 @@ const Info = () => {
         const response = await axios.get(API_URLS.INFO);
         setEventData(response.data);
       } catch (error) {
-        console.log("Error fetching feed:", error);
+        console.log("Error fetching event data:", error);
       } finally {
         setLoading(false);
       }
@@ -35,32 +44,66 @@ const Info = () => {
     fetchData();
   }, []);
 
-  if (loading) return <div>Loading...</div>;
+  if (loading) {
+    return (
+      <div className="lg:grid grid-cols-3 gap-6 lg:space-y-0 space-y-6 p-0 md:p-4 xl:p-6">
+        <div className="col-span-2 space-y-6">
+          <Card className="shadow-lg p-6 rounded-lg">
+            <Skeleton className="h-6 w-40 mb-4" />
+            <Skeleton className="h-4 w-full mb-2" />
+            <Skeleton className="h-4 w-3/4" />
+            <div className="mt-4 space-y-2">
+              <Skeleton className="h-4 w-1/2" />
+              <Skeleton className="h-4 w-2/3" />
+              <Skeleton className="h-4 w-1/4" />
+            </div>
+          </Card>
+        </div>
 
+        <div className="col-span-1 space-y-6">
+          <Skeleton className="w-full h-56 rounded-lg" />
+          <Card className="shadow-lg p-6 rounded-lg">
+            <Skeleton className="h-6 w-40 mb-4" />
+            <div className="mt-4 space-y-2">
+              <Skeleton className="h-4 w-3/4" />
+              <Skeleton className="h-4 w-2/3" />
+              <Skeleton className="h-4 w-1/2" />
+            </div>
+          </Card>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="flex flex-col xl:flex-row gap-10 p-8">
-      <div className="flex-1 space-y-6">
+    <div className="lg:grid grid-cols-3 gap-6 lg:space-y-0 space-y-6 p-0 md:p-4 xl:p-6">
+      <div className="col-span-2 space-y-6">
         <Card className="shadow-lg p-6 rounded-lg">
           <CardTitle className="text-2xl font-bold dark:text-neutral-200 text-neutral-800">
-            About the Event
+            About this Event
           </CardTitle>
-          <p className="text-neutral-600 dark:text-neutral-300">
+          <p className="text-neutral-600 dark:text-neutral-300 mt-2">
             {eventData?.about_event}
           </p>
-          <div className="mt-4 text-neutral-700">
+          <div className="mt-4 text-neutral-700 text-sm dark:text-neutral-300">
             <p>
-              Location: {eventData?.city}, {eventData?.country.name}
+              <strong>Location:</strong> {eventData?.city},
+              {eventData?.country.name}
             </p>
             <p>
-              Date: {eventData?.event_dates.start} to
+              <strong>Date:</strong> {eventData?.event_dates.start} to
               {eventData?.event_dates.end}
             </p>
-            <p>Event Type: {eventData?.event_type}</p>
+            <p>
+              <strong>Event Type: </strong>
+              <span>
+                <Badge>{eventData?.event_type}</Badge>
+              </span>
+            </p>
           </div>
         </Card>
       </div>
 
-      <div className="flex-1 space-y-6">
+      <div className="col-span-1 space-y-6">
         <div className="rounded-lg overflow-hidden shadow-lg">
           <Image
             src={eventData?.poster || "/default-poster.jpg"}
@@ -71,24 +114,33 @@ const Info = () => {
           />
         </div>
 
-        <div className="shadow-lg p-6 rounded-lg">
+        <Card className="shadow-lg p-6 rounded-lg">
           <CardTitle className="text-xl font-semibold dark:text-neutral-200 text-neutral-800">
             Important Dates
           </CardTitle>
-          <div className="space-y-4 mt-4">
+          <div className="mt-4 space-y-4">
             {Object.entries(eventData?.important_dates || {}).map(
               ([title, date], index) => (
                 <div
                   key={index}
-                  className="flex justify-between text-neutral-600 dark:text-neutral-300"
+                  className="flex items-center border-l-2 border-gray-300 dark:border-neutral-600 pl-4"
                 >
-                  <span>{title}</span>
-                  <span>{date}</span>
+                  <div className="relative left-[-1.65rem] dark:bg-neutral-900 rounded-full">
+                    <CalendarDays className="w-5 h-5 p-1 text-neutral-500 dark:text-neutral-400" />
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="text-sm font-medium dark:text-neutral-200 text-neutral-700">
+                      {title}
+                    </span>
+                    <div className="dark:bg-neutral-900 w-full px-3 py-1 rounded-[1rem]">
+                      <p className="text-xs">{date}</p>
+                    </div>
+                  </div>
                 </div>
               )
             )}
           </div>
-        </div>
+        </Card>
       </div>
     </div>
   );
--- app/api/home/info/route.ts
+++ app/api/home/info/route.ts
@@ -4,10 +4,10 @@ const info = {
   poster:
     "https://events.avisengine.com/_next/image?url=https%3A%2F%2Fapi.avisengine.com%2Fassets%2F335a471c-3df6-42f3-847f-a1b6cb084e6f&w=1080&q=75",
   important_dates: {
-    "Technical Documents Submission": "2025-01-26 12:00 (Asia/Tehran Time)",
-    "Early Registration": "2025-02-19 23:59 (Asia/Tehran Time)",
-    "Regular Registration": "2025-03-01 23:59 (Asia/Tehran Time)",
-    "Late Registration": "2025-03-10 23:59 (Asia/Tehran Time)",
+    "Technical Documents Submission": "2025-01-26 12:00 (Asia/Tehran)",
+    "Early Registration": "2025-02-19 23:59 (Asia/Tehran)",
+    "Regular Registration": "2025-03-01 23:59 (Asia/Tehran)",
+    "Late Registration": "2025-03-10 23:59 (Asia/Tehran)",
   },
   about_event:
     "The National Robotics Committee of the Islamic Republic of Iran (FIRA) and the Iranian Robotics Association play an effective role in the fundamental advancement of the development and promotion goals of the sciences and skills required in the robotics, mechatronics, and artificial intelligence industry, annually The international robotics and artificial intelligence competitions (Iran FIRA Cup) will be held in different leagues in two categories, youth, and pro. All professors, researchers, students, and those interested in the fields of robotics, mechatronics, and artificial intelligence are invited to participate in this festival and help us in holding this conference magnificently and effectively, as well as presenting and exchanging information and scientific and Industrial achievements. The National Robotics Committee of the Islamic Republic of IranFIRA, The Robotics Society of Iran, and the FIRA World Robotics Federation (FIRA) wish success to all the participants of this competition. The first FIRA Programming competition in the two disciplines of ScratchBot and PyCraft will be held at this event.",
@@ -21,6 +21,16 @@ const info = {
   },
   city: "Tehran",
   event_type: "In-person",
+  organizers: [
+    {
+      name: "FIRA",
+      logo: "https://events.avisengine.com/_next/image?url=https%3A%2F%2Fapi.avisengine.com%2Fassets%2F5882f184-db34-44d7-93b3-944d274115a9&w=384&q=75",
+    },
+    {
+      name: "Avis Engine",
+      logo: "https://events.avisengine.com/_next/image?url=https%3A%2F%2Fapi.avisengine.com%2Fassets%2F95b53f64-b8fc-427b-8a9f-5f800da4e0c3&w=384&q=75",
+    },
+  ],
 };
 
 export async function GET() {
